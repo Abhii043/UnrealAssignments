4ffd44fe153c57be7953d9f3b0f78a894926530f
Abhi <abhi.np.71@gmail.com>
2024-06-06 12:43:44 +0530

    Assignment-6 Started

---
+++ Plugins/Assignment_5_6/Source/Assignment_5_6/Private/SelectionAreaController.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SelectionAreaController.h"
+
+void ASelectionAreaController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (_SelectionActor) {
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		FVector Location = FVector::ZeroVector;
+		SelectionActor = GetWorld()->SpawnActor<ASelectionArea>(_SelectionActor ,Location , FRotator::ZeroRotator , SpawnParams);
+		SelectionActor->GenerateCube({100,100,100});
+	}
+	bShowMouseCursor = true;
+}
+
+void ASelectionAreaController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FCollisionQueryParams TraceParams(FName(TEXT("LineTrace")), true, SelectionActor);
+
+	FVector CursorWorldLocation;
+	FVector CursorWorldDirection;
+	DeprojectMousePositionToWorld(CursorWorldLocation, CursorWorldDirection);
+
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, CursorWorldLocation, CursorWorldLocation + CursorWorldDirection * 10000, ECC_Visibility, TraceParams)) {
+		FVector Location = HitResult.Location;
+
+		FVector NewDimensions = SelectionActor->GetActorScale3D() * ((SelectedBoundingBox == "Box") ?  Dimensions : FVector(Radius, Radius, Radius));
+
+		Location.Z = (SelectedBoundingBox == "Box") ? NewDimensions.Z / 2 : NewDimensions.Z;
+		SelectionActor->SetActorLocation(Location);
+	}
+}
+
+void ASelectionAreaController::SelectBoundingShape(const FString& ShapeType)
+{
+	SelectedBoundingBox = ShapeType;
+	if (ShapeType == "Sphere") {
+		if (SelectionActor) {
+			SelectionActor->Destroy();
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+			FVector Location = FVector::ZeroVector;
+			SelectionActor = GetWorld()->SpawnActor<ASelectionArea>(_SelectionActor, HitResult.Location, FRotator::ZeroRotator, SpawnParams);
+			SelectionActor->GenerateSphere(Radius , 32 ,32);
+		}
+	}
+	else if (ShapeType == "Box") {
+		if (SelectionActor) {
+			SelectionActor->Destroy();
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+			FVector Location = FVector::ZeroVector;
+			SelectionActor = GetWorld()->SpawnActor<ASelectionArea>(_SelectionActor, HitResult.Location, FRotator::ZeroRotator, SpawnParams);
+			SelectionActor->GenerateCube(Dimensions);
+		}
+	}
+}
+
+void ASelectionAreaController::ChangeScale(const FString& Property, const float& Value)
+{
+	FVector Scale = SelectionActor->GetActorScale3D();
+	if(Property == "Radius"){
+		SelectionActor->SetActorScale3D(FVector(Value , Value , Value)/Radius);
+	}
+	else if(Property == "DimensionX") {
+		SelectionActor->SetActorScale3D(FVector(Value / Dimensions.X, Scale.Y, Scale.Z));
+	}
+	else if (Property == "DimensionY") {
+		SelectionActor->SetActorScale3D(FVector(Scale.X, Value / Dimensions.Y, Scale.Z));
+	}
+	else if (Property == "DimensionZ") {
+		SelectionActor->SetActorScale3D(FVector(Scale.X, Scale.Y, Value / Dimensions.Z));
+	}
+}
---
+++ Plugins/Assignment_5_6/Source/Assignment_5_6/Public/MeshDataAsset.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MeshDataAsset.generated.h"
+
+UCLASS()
+class ASSIGNMENT_5_6_API UMeshDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties")
+	UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties")
+	float MinScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties")
+	float MaxScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties")
+	float MinRotation;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties")
+	float MaxRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MeshProperties" , Meta = (ClampMin = 1 , UIMin = 1 , UIMax = 10 , ClampMax = 10))
+	float footPrint;
+};
